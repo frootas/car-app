a77c8afd0a68b22fb408d1a6a1232dd8a1a49e9b
frootas <frootas234@gmail.com>
2020-05-14 14:19:04 +1000

    Updates

---
+++ precache-manifest.de88d919edf4f880b1e449bdcf29d687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc70fb29e13463ef9d46935fde6a85a",
+    "url": "/car-app/index.html"
+  },
+  {
+    "revision": "3869da092d1678686324",
+    "url": "/car-app/static/css/main.a2233545.chunk.css"
+  },
+  {
+    "revision": "cc2bcf087ef6653a3187",
+    "url": "/car-app/static/js/2.a8d23e69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/car-app/static/js/2.a8d23e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3869da092d1678686324",
+    "url": "/car-app/static/js/main.ef49057f.chunk.js"
+  },
+  {
+    "revision": "d4fd46011ef2c0713c1e",
+    "url": "/car-app/static/js/runtime-main.bb00d5a8.js"
+  }
+]);
